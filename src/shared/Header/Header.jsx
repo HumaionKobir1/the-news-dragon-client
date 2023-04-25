c75c32a7601222dc200a46d44fedb566de206809
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-04-25 23:17:51 +0600

    Create Layout, Header components and moments js

---
+++ src/shared/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../../assets/logo.png'
+import moment from 'moment';
+
+
+const Header = () => {
+    return (
+        <div>
+            <div className='text-center'>
+                <img src={logo} alt="" />
+                <p className='text-secondary'><small>Journalism Without Fear or Favour</small></p>
+                <p>{moment().format("dddd, MMMM D, YYYY")}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
