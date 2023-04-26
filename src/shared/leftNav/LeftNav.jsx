46fe6b4931d27b5e04bfd5ba850624a0972cd3b8
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-04-26 12:25:19 +0600

    Install node-exporess and run your server

--- src/shared/leftNav/LeftNav.jsx
+++ src/shared/leftNav/LeftNav.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const LeftNav = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/categories')
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
+    }, [])
     return (
         <div>
             <h1>lef nav</h1>
