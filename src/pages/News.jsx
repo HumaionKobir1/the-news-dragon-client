1ccc21e2286fbbf932ee009e12fa434743d0688e
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-04-27 13:36:04 +0600

    Display News Details and editors insights

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { FaArrowLeft } from 'react-icons/fa';
+import { Link, useLoaderData } from 'react-router-dom';
+import EiditorsInsights from './EiditorsInsights';
 
 const News = () => {
+    const news = useLoaderData()
+    const {_id, title, details, image_url, category_id} = news;
+
     return (
         <div>
-            <h1>news details coming soon.....</h1>
+            <Card>
+                <Card.Img variant="top" src={image_url} />
+                    <Card.Body>
+                        <Card.Title>{title}</Card.Title>
+                        <Card.Text>
+                            {details}
+                        </Card.Text>
+                        <Link to={`/category/${category_id}`}>
+                            <Button variant='danger'><FaArrowLeft></FaArrowLeft> All news in this categories</Button>
+                        </Link>
+                    </Card.Body>
+            </Card>
+            <EiditorsInsights></EiditorsInsights>
         </div>
     );
 };
